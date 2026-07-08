84294170718b42a1d565257ee4b959fd62eb6106
arshan97 <arshanshaikh97@gmail.com>
2026-07-09 00:36:28 +0530

    made the env package work

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/arshan97/rss-aggregator
 
 go 1.25.0
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("hello world!")
+
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+
+	fmt.Print("Port:", portString)
 }
