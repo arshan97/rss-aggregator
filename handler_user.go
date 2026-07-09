1253b00733714f0aa68553321e6921a3715a8787
arshan97 <arshanshaikh97@gmail.com>
2026-07-09 19:35:01 +0530

    add user API key management with create and retrieve functionality

--- handler_user.go
+++ handler_user.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/arshan97/rss-aggregator/internal/auth"
 	"github.com/arshan97/rss-aggregator/internal/database"
 	"github.com/google/uuid"
 )
@@ -38,4 +39,20 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	}
 
 	respondWithJSON(w, 201, databaseUsertoUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 401, fmt.Sprintf("Unauthorized: %s", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByApiKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 404, fmt.Sprintf("User not found: %s", err))
+		return
+	}
+
+	respondWithJSON(w, 200, databaseUsertoUser(user))
 }
